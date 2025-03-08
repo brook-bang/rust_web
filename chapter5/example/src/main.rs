4b6e61f589dd3e74a3e57ad7f1718dff21decf48
brook-bang <1455047806@qq.com>
2025-03-08 21:27:08 +0800

    add

--- chapter5/example/src/main.rs
+++ chapter5/example/src/main.rs
@@ -1,12 +1,17 @@
 #![warn(clippy::all)]
 
+use std::any;
+
 use handle_errors::return_error;
-use warp::{http::Method, Filter};
+use warp::{filters::any::any, http::Method, Filter};
 
 mod routes;
 mod store;
 mod types;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let store = store::Store::new();
+    let store_filer = warp:any().map(move || store.clone());
+    let cors = warp.c
 }
